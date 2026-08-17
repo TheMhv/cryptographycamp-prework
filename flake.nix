a0c1132839cc2e5e7b9754f8a22bd1389f9b5fe1
TheMhv <TheMhv@proton.me>
2026-08-17 09:39:19 -0300

    setup: Minimal setup for development

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Simple Haskell Dev Environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ghc
+            cabal-install
+          ];
+        };
+      }
+    );
+}
